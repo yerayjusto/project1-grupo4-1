b9a7c62bf537bbb088613affdf07cde9da1c10f8
Carlos Cardona <charlie@emitus.com>
2021-05-24 15:25:12 +0100

    Enemy moved

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ function Player (top, left, id) {
   this.top = top
   this.left = left
   this.elem = id
-  this.direction = 0 // 0 stop; 1 up ; 2 right; 3 down; 4 left
   this.distance = 10
 }
 
@@ -16,7 +15,7 @@ Player.prototype.move = function (value) {
       // Si tengo colision con el canvas
       if (this.top - this.distance >= 0) {
         // Si tengo colision con el enemigo
-        if (colision(enemy, this.distance, 0, 0, 0) === false) {
+        if (colision(browser, this.distance, 0, 0, 0) === false) {
           // Si tengo colision con el goal
           if (colision(goal, this.distance, 0, 0, 0) === true) {
             console.log('Win')
@@ -28,7 +27,7 @@ Player.prototype.move = function (value) {
       break
     case 'ArrowRight':
       if (this.left + this.distance + this.width <= canvas.width) {
-        if (colision(enemy, 0, this.distance, 0, 0) === false) {
+        if (colision(browser, 0, this.distance, 0, 0) === false) {
           if (colision(goal, 0, this.distance, 0, 0) === true) {
             console.log('Win')
           } else {
@@ -39,7 +38,7 @@ Player.prototype.move = function (value) {
       break
     case 'ArrowDown':
       if (this.top + this.distance + this.height <= canvas.height) {
-        if (colision(enemy, 0, 0, this.distance, 0) === false) {
+        if (colision(browser, 0, 0, this.distance, 0) === false) {
           if (colision(goal, 0, 0, this.distance, 0) === true) {
             console.log('Win')
           } else {
@@ -50,9 +49,9 @@ Player.prototype.move = function (value) {
       break
     case 'ArrowLeft':
       if (this.left - this.distance >= 0) {
-        if (colision(enemy, 0, 0, 0, this.distance) === false) {
+        if (colision(browser, 0, 0, 0, this.distance) === false) {
           if (colision(goal, 0, 0, 0, this.distance) === true) {
-            console.log('Win') 
+            console.log('Win')
           } else {
             this.left -= this.distance
           }
@@ -71,11 +70,32 @@ const canvas = {
   height: 400
 }
 
-const enemy = {
-  top: 100,
-  left: 100,
-  width: 40,
-  height: 60
+function Enemy (top, left, id) {
+  this.width = 40
+  this.height = 60
+  this.top = top
+  this.left = left
+  this.elem = id
+  this.distance = 1
+}
+
+Enemy.prototype.move = function (value) {
+  switch (value) {
+    case 1:
+      this.top -= this.distance
+      break
+    case 2:
+      this.left += this.distance
+      break
+    case 3:
+      this.top += this.distance
+      break
+    case 4:
+      this.left -= this.distance
+      break
+  }
+  this.elem.style.top = this.top + 'px'
+  this.elem.style.left = this.left + 'px'
 }
 
 const goal = {
@@ -99,9 +119,30 @@ function colision (collidedObj, up, right, down, left) {
 // Instancia
 
 const playerElement = document.getElementById('player')
+const enemyElement = document.getElementById('enemy')
 
 const mario = new Player(10, 10, playerElement)
+const browser = new Enemy(100, 100, enemyElement)
 
+// Prueba movimiento
+let i = 1
+let count = 1
+
+setInterval(function () {
+  if (count <= 100) {
+    browser.move(i)
+    count++
+  } else {
+    i++
+    if (i > 4) {
+      i = 1
+    }
+    count = 1
+  }
+}, 20)
+
+// Teclas
 window.addEventListener('keydown', function (e) {
   mario.move(e.code)
 })
+
